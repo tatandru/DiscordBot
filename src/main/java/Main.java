6c3b0c8f74ee5a0d8a012d8b4effec8d0666d462
tatandru <48557706+tatandru@users.noreply.github.com>
2021-03-05 21:39:21 +0200

    making the bot online

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,20 @@
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+
 public class Main {
+
+    private final static String TOKEN = "ODE3NDYzNDA2MzA2MjYzMDcy.YEJ4Hw.9O6cBFliL-lDid5FiyPDoUVkc04";
 
     public static void main(String[] args) {
 
+        GatewayDiscordClient client = DiscordClientBuilder.create(TOKEN)
+                .build()
+                .login()
+                .block();
     }
 
 }
