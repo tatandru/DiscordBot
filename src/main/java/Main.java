05489b2148684c5362e63bdc8a07e7af4efeec00
tatandru <48557706+tatandru@users.noreply.github.com>
2021-03-10 02:22:53 +0200

    Changed the library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,9 @@
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import discord4j.common.util.Snowflake;
-import discord4j.core.DiscordClientBuilder;
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.User;
-import discord4j.core.object.entity.channel.MessageChannel;
-import discord4j.core.retriever.EntityRetrievalStrategy;
-import discord4j.core.spec.MessageCreateSpec;
-import discord4j.discordjson.json.*;
-import discord4j.discordjson.json.gateway.MessageCreate;
-import discord4j.discordjson.possible.Possible;
-import discord4j.gateway.ShardInfo;
-import discord4j.rest.entity.RestChannel;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.time.Instant;
-import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.function.Consumer;
+
 
 
 public class Main {
@@ -39,10 +15,7 @@ public class Main {
         Scanner scanner =new Scanner(file);
         final String TOKEN = scanner.nextLine();
 
-        GatewayDiscordClient client = DiscordClientBuilder.create(TOKEN)
-                .build()
-                .login()
-                .block();
+        DiscordApi api = new DiscordApiBuilder().setToken(TOKEN).login().join();
     }
 
 }
