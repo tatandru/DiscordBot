025cf7c9891f864d604528578189f8aaf85a3f90
tatandru <48557706+tatandru@users.noreply.github.com>
2021-03-10 18:52:36 +0200

    Make a !clean command that clears last 10 messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.util.cache.MessageCache;
 
 import java.io.*;
 import java.util.Scanner;
@@ -14,8 +17,18 @@ public class Main {
         File file = new File("src/token.txt");
         Scanner scanner =new Scanner(file);
         final String TOKEN = scanner.nextLine();
+        MessageBuilder message= new MessageBuilder().setContent("!clean");
 
         DiscordApi api = new DiscordApiBuilder().setToken(TOKEN).login().join();
+
+        api.addMessageCreateListener(event -> {
+            if(event.getMessageContent().equals("!abc")){ ;
+                event.getChannel().getMessageCache().setCapacity(10);
+               api.getCachedMessages().deleteAll();
+               event.getChannel().sendMessage("cleaned last 10");
+            }
+        });
+
     }
 
 }
