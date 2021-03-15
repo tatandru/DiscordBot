38aac5b0f4cd94f5b37eecb5df6cf7869b2bfb67
tatandru <48557706+tatandru@users.noreply.github.com>
2021-03-15 19:08:00 +0200

    added a help commands and added all triggers in the same listener

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.activity.ActivityType;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
@@ -29,31 +30,37 @@ public class Main {
 
 
         DiscordApi api = new DiscordApiBuilder().setToken(TOKEN).login().join();
+        api.updateActivity("Use !girthicel for commands");
 
         api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equals("!clean10")) {
-
+            TextChannel channel = event.getChannel();
+            String trigger = event.getMessageContent();
+            if (trigger.startsWith("!wolfram")) {
+                if (trigger.length() > 8) {
+                    try {
+                        File imageResponse = WolframAlphaSimpleApi.apiRequest(wolframApiKey, trigger.substring(8));
+                        channel.sendMessage(imageResponse);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    channel.sendMessage(new EmbedBuilder().setDescription("Try like this-> !wolfram <query>"));
+                }
+            }
+            if (trigger.startsWith("!girthicel")) {
+                new MessageBuilder().setEmbed(
+                        new EmbedBuilder()
+                                .setTitle("Girthicel Commands")
+                                .addField("!wolfram <query>", "Use this command to search curiosities or solve math stuff")
+                                .addField("!clean10", "Clean only the last 10 messages")).send(channel);
+            }
+            if(trigger.startsWith("!clean10")){
                 event.getChannel().getMessageCache().setCapacity(10);
                 api.getCachedMessages().deleteAll();
                 event.getChannel().sendMessage("Executed !clean10");
             }
         });
-        api.addMessageCreateListener(event -> {
-            TextChannel channel=event.getChannel();
-            String trigger=event.getMessageContent();
-            if (trigger.startsWith("!wolfram")) {
-                if(trigger.length()>8){
-                    try {
-                        File imageResponse= WolframAlphaSimpleApi.apiRequest(wolframApiKey,trigger.substring(8));
-                        channel.sendMessage(imageResponse);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }else{
-                    channel.sendMessage(new EmbedBuilder().setDescription("Try like this-> !wolfram <query>"));
-                }
-            }
-        });
+
 
     }
 
