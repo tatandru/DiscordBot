e4119002b679b7836d76e48ad2cac5a4143c8e32
tatandru <48557706+tatandru@users.noreply.github.com>
2021-03-26 20:01:15 +0200

    Revert "made it read the keys from environment table"
    This reverts commit e4978bdd9a23e89658ae512e3fc38112e7d959db.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,8 +45,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
+        System.out.println(TOKEN);
         initDB();
         MongoCollection<Curse> cursesCollection = db.getCollection("curses", Curse.class);
         Curse curse = cursesCollection.find().first();
